d5167b5d290b72078a893328523d9b18001f7b52
Benedikt Tobias <61744142+benedikttobias@users.noreply.github.com>
2023-01-25 18:22:13 +0100

    Updated date-range style

--- src/components/RawDataPlot.tsx
+++ src/components/RawDataPlot.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { useContext, useState } from "react";
-import Plotly, { Layout, PlotData } from "plotly.js-basic-dist-min";
+import {useContext, useState} from "react";
+import Plotly, {Layout, PlotData} from "plotly.js-basic-dist-min";
 import createPlotlyComponent from "react-plotly.js/factory";
-import { Alert, Theme, useTheme } from "@mui/material";
-import { DateRange, Sensor } from "../App";
-import { PlotConfig, PlotLayout, rangeFromRelayoutEvent, ZoomHint } from "./PlotlyUtils";
-import { MessagingContext } from "./MessagingContext";
+import {Alert, Theme, useTheme} from "@mui/material";
+import {DateRange, Sensor} from "../App";
+import {PlotConfig, PlotLayout, rangeFromRelayoutEvent, ZoomHint} from "./PlotlyUtils";
+import {MessagingContext} from "./MessagingContext";
 
 type RawDataProps = {
     showHint: boolean;
@@ -24,7 +24,7 @@ function prepareLayout(theme: Theme, range: { x: string[]; y: string[] }): Parti
     let layoutBuilder = new PlotLayout(lightTheme)
         .withTitle("Sensor data")
         .withLineColors(colors)
-        .withMargins({ l: 20, r: 20, b: 30, t: 30 })
+        .withMargins({l: 20, r: 20, b: 30, t: 30})
         .withLegend();
     let layout = layoutBuilder.build();
 
@@ -58,7 +58,7 @@ function prepareData(
 function RawDataPlot(props: RawDataProps) {
     const theme = useTheme();
     const messageSink = useContext(MessagingContext);
-    const [range, setRange] = useState<{ x: string[]; y: string[] }>({ x: [], y: [] });
+    const [range, setRange] = useState<{ x: string[]; y: string[] }>({x: [], y: []});
 
     if (props.showHint) {
         return (
@@ -71,27 +71,74 @@ function RawDataPlot(props: RawDataProps) {
     let layout = prepareLayout(theme, range);
 
     const dateRangeDefined = !Object.entries(props.dateRange).some((kv) => kv[1] === null);
-    if (
-        dateRangeDefined &&
-        (props.dateRange.start! > props.dateRange.min! || props.dateRange.end! < props.dateRange.max!)
-    ) {
-        layout.shapes = [
-            {
-                type: "rect",
-                xref: "x",
-                x0: props.dateRange.start,
-                x1: props.dateRange.end,
-                yref: "paper",
-                y0: 0,
-                y1: 1,
-                line: {
-                    color: "rgba(255,255,255,0.9)",
-                    width: 1,
+
+    const lineWidth = 2;
+    const lineColor = "rgba(150,150,150,1)";
+    const areaColor = "rgba(150,150,150,0.5)";
+
+    if (dateRangeDefined) {
+        let shapes = []
+        if (props.dateRange.start! > props.dateRange.min!) {
+            shapes = shapes.concat([
+                {
+                    type: "rect",
+                    xref: "x",
+                    x0: props.dateRange.min,
+                    x1: props.dateRange.start,
+                    yref: "paper",
+                    y0: 0,
+                    y1: 1,
+                    line: {
+                        width: 0
+                    },
+                    fillcolor: areaColor,
                 },
-                fillcolor: "rgba(255,255,255,0.1)",
-                layer: "below",
-            },
-        ];
+                {
+                    type: "line",
+                    x0: props.dateRange.start,
+                    x1: props.dateRange.start,
+                    yref: "paper",
+                    y0: 0,
+                    y1: 1,
+                    line: {
+                        color: lineColor,
+                        width: lineWidth,
+                    },
+                }
+            ]);
+        }
+
+        if (props.dateRange.end! < props.dateRange.max!) {
+            shapes = shapes.concat([
+                {
+                    type: "rect",
+                    xref: "x",
+                    x0: props.dateRange.end,
+                    x1: props.dateRange.max,
+                    yref: "paper",
+                    y0: 0,
+                    y1: 1,
+                    line: {
+                        width: 0
+                    },
+                    fillcolor: areaColor,
+                },
+                {
+                    type: "line",
+                    x0: props.dateRange.end,
+                    x1: props.dateRange.end,
+                    yref: "paper",
+                    y0: 0,
+                    y1: 1,
+                    line: {
+                        color: lineColor,
+                        width: lineWidth,
+                    },
+                }
+            ]);
+        }
+
+        layout.shapes = shapes;
     }
 
     const Plot = createPlotlyComponent(Plotly);
@@ -100,11 +147,11 @@ function RawDataPlot(props: RawDataProps) {
             data={prepareData(props.timestamps, props.timeseries, props.sensors)}
             layout={layout}
             config={PlotConfig}
-            style={{ width: "100%", height: "100%" }}
+            style={{width: "100%", height: "100%"}}
             onRelayout={(e) => {
                 setRange(rangeFromRelayoutEvent(e, range));
                 if (e["xaxis.autorange"] === undefined && !props.zoomHintShown) {
-                    messageSink({ severity: "info", message: ZoomHint, timeout: 4000 });
+                    messageSink({severity: "info", message: ZoomHint, timeout: 4000});
                     props.onZoomHint();
                 }
             }}
